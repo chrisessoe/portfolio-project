2e100fb7a2a5bafb4cb91ada1a0d3b598e6aceda
Chris Essoe <chrisessoe@gmail.com>
2021-02-10 19:02:11 -0800

    commit 2

--- Documents/portfolioProject/js/scripts.js
+++ Documents/portfolioProject/js/scripts.js
@@ -1,31 +1,7 @@
   $(function () {
-        $(".carousel").carousel({ interval: 2000 });
-            $("#carouselButton").click(function () {
-                if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-        $(".carousel").carousel("pause");
-                    $("#carouselButton").children("i").removeClass("fa-pause");
-                    $("#carouselButton").children("i").addClass("fa-play");
-                } else {
-        $(".carousel").carousel("cycle");
-                    $("#carouselButton").children("i").removeClass("fa-play");
-                    $("#carouselButton").children("i").addClass("fa-pause");
-                }
-            });
-  });
+        $(".carousel").carousel({ interval: 5000 });
+   });
         
-$('#reserveButton').click(function () {
-    $('#reserveModal').modal('show');
-});
-
-$('#loginButton').click(function () {
-    $('#loginModal').modal('show');
-});
-
-
-$(function () {
-        $('[data-toggle="tooltip"]').tooltip()
-    })
-
 $(function () {
             $(document).scroll(function () {
                 var $nav = $("#mainNavbar");
